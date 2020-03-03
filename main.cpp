e8e90e23e9935508459b1b5773018e71668c9071
stanislav-sem <crazykvark@gmail.com>
2020-03-03 21:07:50 +0000

    main.cpp ready for ex3

--- main.cpp
+++ main.cpp
@@ -1,38 +1,50 @@
-#include<iostream>
+#include <iostream>
+#include <string>
+#include "brass.h"
 using namespace std;
-#include "classic.h"           // будет содержать #include cd.h
-
-void Bravo(const Cd & disk);
+const int CLIENTS = 4;
 
 int main() {
-    Cd c1("Beatles", "Capitol", 14, 35.5);
-    Classic c2 = Classic("Piano Sonata in B flat, Fantasia in C", 
-                         "Alfred Brendel", "Philips", 2, 57.17);
-    Cd *pcd = &c1;
-    // Непосредственное использование объектов
-    cout << "Using object directly:\n";
-    c1.Report();                // Использование метода Cd
-    c2.Report();                // использование метода Classic
-
-    // Использование указателя на объекты типа cd *
-    cout << "Using type cd* pointer to objects:\n";
-    pcd->Report();              // использование метода Cd для объекта cd
-    pcd = &c2;
-    pcd->Report();              // использование метода Classic для объекта classic
-
-    // Вызов функции с аргументом-ссылкой на Cd
-    cout << "Calling a function with a Cd reference argument:\n";
-    Bravo(c1);
-    Bravo(c2);
+    Brass * p_clients[CLIENTS];
+    string temp;
+    long tempnum;
+    double tempbal;
+    char kind;
+    for (int i = 0; i < CLIENTS; i++) {
+        cout << "Enter client's name: "
+        getline(cin, temp);
+        cout << "Enter clint's account number: ";
+        cin >> tempnum;
+        cout << "Enter opening balance: $";
+        cin >> tempbal;
+        cout << "Entet 1 for Brass Account or "
+             << "2 for BrassPlus Account: ";
+        while (cin >> kind && (kind != '1' && kind != '2'))
+            cout << "Enter 1 or 2: ";
+        if (kind == '1') {
+            p_clients[i] = new Brass(temp, tempnum, tempbal);
+        } else {
+            double tmax, trate;
+            cout << "Enter the overdraft limit: $";
+            cin >> tmax;
+            cout << "Enter the interest rate"
+                 << "as a decimal fraction: ";
+            cin >> trate;
+            p_clients[i] = new BrassPlus(temp, tempnum, tempbal, tmax, trate);
+        }
+        while (cin.get() != '\n') {
+            continue;
+        }
+    }
+    cout << endl;
+    for (int i = 0; i < CLIENTS; i++ ) {
+        p_clients[i]->ViewAcct();
+        cout << endl;
+    }
+    for (int i = 0; i < CLIENTS; i++ ) {
+        delete p_clients[i];
+    }
+    cout << "Done.\n";
     
-    // Тестирование присваивания
-    cout << "Testing assignment: ";
-    Classic copy;
-    copy = c2;
-    copy.Report();
     return 0;
-}
-
-void Bravo(const Cd & disk) {
-    disk.Report();
 }
