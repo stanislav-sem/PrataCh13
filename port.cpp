d24f1282343a77222f14a4fad7229eed3d66d7e6
stanislav-sem <crazykvark@gmail.com>
2020-03-04 20:07:37 +0000

    port.cpp ready

--- port.cpp
+++ port.cpp
@@ -54,3 +54,46 @@ ostream & operator<<(ostream & os, const Port & p) {
        << p.bottles << endl;
     return os;
 }
+
+// методы VintagePort -----------------------------------------------
+VintagePort::VintagePort() : Port("none", "vintage", 0) {
+    nickname = nullptr;
+    year = 666;
+}
+
+VintagePort::VintagePort(const char * br, int b, const char * nn, int y) 
+            :Port(br, "vintage", b) {
+    nickname = new char[strlen(nn) + 1];
+    strcpy(nickname, nn);
+    year = y;
+}
+
+VintagePort::VintagePort(const VintagePort & vp) : Port(vp) {
+    nickname = new char[strlen(vp.nickname) + 1];
+    strcpy(nickname, vp.nickname);
+    year = vp.year;
+}
+
+VintagePort & VintagePort::operator=(const VintagePort & vp) {
+    if (this == &vp) {
+        return *this;
+    }    
+    Port::operator=(vp);
+    delete [] nickname;
+    nickname = new char[strlen(vp.nickname) + 1];
+    strcpy(nickname, vp.nickname);
+    year = vp.year;
+    return *this;
+}
+
+void VintagePort::Show() const {
+    cout << "Nickname: " << nickname 
+         << "Year: " << year << endl;
+    Port::Show();
+}
+
+ostream & operator<<(ostream & os, const VintagePort & vp) {
+    os << vp.nickname << ", " << vp.year << ", "
+       << (const Port &) vp << endl;
+    return os;
+}
